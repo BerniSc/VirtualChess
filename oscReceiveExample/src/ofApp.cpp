3b68b50370b7d5df464c7a129525c36ae6c47023
jasmina <jasmina.stich@yahoo.de>
2023-11-28 08:23:27 +0100

    oscReceiver

---
+++ oscReceiveExample/src/ofApp.cpp
@@ -0,0 +1,124 @@
+#include "ofApp.h"
+#include "math.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetWindowTitle("oscReceiveExample");
+	//ofSetFrameRate(60); // run at 60 fps
+
+	// listen on the given port
+	ofLog() << "listening for osc messages on port " << PORT;
+	receiver.setup(PORT);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+	// check for waiting messages
+	while(receiver.hasWaitingMessages()){
+
+		// get the next message
+		ofxOscMessage m;
+		receiver.getNextMessage(m);
+		ofLog() << m.getAddress();
+
+		// check for mouse moved message
+		if(m.getAddress() == "/daumen"){
+
+			// both the arguments are floats
+			mouseXfD = m.getArgAsFloat(1);
+			mouseYfD = m.getArgAsFloat(2);
+			ofLog() << mouseXfD << " " << mouseYfD;
+		}
+		if(m.getAddress() == "/zeigefinger"){
+
+			// both the arguments are floats
+			mouseXfZ = m.getArgAsFloat(1);
+			mouseYfZ = m.getArgAsFloat(2);
+			ofLog() << mouseXfZ << " " << mouseYfZ;
+		}
+	}
+
+	checkDistance();
+}
+
+
+bool ofApp::checkDistance() const {
+	float distance = sqrt((mouseXfZ - mouseXfD)*(mouseXfZ - mouseXfD) + (mouseYfZ - mouseYfD)*(mouseYfZ - mouseYfD));
+	ofLog() << "Distance: " << distance;
+
+	if (distance <= 30){
+		ofLog() << "True";
+		return true;
+	}
+	
+	if (distance >= 40){
+		ofLog() << "False";
+		return false;
+	}
+	
+	return false;
+}
+
+
+
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+
+}
---
+++ oscReceiveExample/src/ofApp.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+
+// listening port
+#define PORT 5005
+
+// max number of strings to display
+#define NUM_MSG_STRINGS 20
+
+// demonstrates receiving and processing OSC messages with an ofxOscReceiver,
+// use in conjunction with the oscSenderExample
+class ofApp : public ofBaseApp{
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		bool checkDistance() const;
+
+		ofTrueTypeFont font;
+		ofxOscReceiver receiver;
+
+		int currentMsgString;
+		string msgStrings[NUM_MSG_STRINGS];
+		float timers[NUM_MSG_STRINGS];
+
+		float mouseXfD = 0;
+		float mouseYfD = 0;
+		float mouseXfZ = 0;
+		float mouseYfZ = 0;
+		int mouseButtonInt = 0;
+		string mouseButtonState = "";
+
+		ofImage receivedImage;
+};

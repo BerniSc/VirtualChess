1caac0386ead428447be72772beafedbbff55f37
jasmina <jasmina.stich@yahoo.de>
2024-01-10 10:30:09 +0100

    Client-Server Tausch

--- server.py
+++ server.py
@@ -1,37 +1,148 @@
-"""Small example OSC server
-
-This program listens to several addresses, and prints some information about
-received packets.
-"""
 import argparse
-import math
 
-from pythonosc.dispatcher import Dispatcher
-from pythonosc import osc_server
+from pythonosc import udp_client
+import cv2
+import mediapipe as mp
 
-def print_volume_handler(unused_addr, args, volume):
-  print("[{0}] ~ {1}".format(args[0], volume))
+import numpy as np
 
-def print_compute_handler(unused_addr, args, volume):
-  try:
-    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
-  except ValueError: pass
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# Fkt. / Vars für Kalibrierung
+# Global variables
+points = []
+clicked = False
+
+
+# The Order in which the points have to be clicked is TL TR BR BL
+# Callback function for mouse events
+def mouse_callback(event, x, y, flags, param):
+    global clicked, points
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points.append((x, y))
+        clicked = True
+        if len(points) == 4:
+            clicked = False
+
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--ip",
-      default="127.0.0.1", help="The ip to listen on")
-  parser.add_argument("--port",
-      type=int, default=5005, help="The port to listen on")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+                        help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=5005,
+                        help="The port the OSC server is listening on")
+    args = parser.parse_args()
 
-  dispatcher = Dispatcher()
-  dispatcher.map("/daumen", print)
-  dispatcher.map("/zeigefinger", print)
-  dispatcher.map("/volume", print_volume_handler, "Volume")
-  dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    clientB = udp_client.SimpleUDPClient("100.83.140.1", args.port)
 
-  server = osc_server.ThreadingOSCUDPServer(
-      (args.ip, args.port), dispatcher)
-  print("Serving on {}".format(server.server_address))
-  server.serve_forever()
+    # Kalibrierung
+    # Capture video from the default camera
+    cap = cv2.VideoCapture(2)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    # Create a window and set the mouse callback function
+    cv2.namedWindow("Perspective Transform")
+    cv2.setMouseCallback("Perspective Transform", mouse_callback)
+
+    # Terminate with Ctrl-C after calling like python3 pythontest.yp or by pressing ESC
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        if clicked and len(points) < 4:
+            cv2.circle(frame, points[-1], 5, (0, 255, 0), -1)
+
+        if len(points) == 4:
+            # Define the target points for the perspective transform
+            target_points = np.array([(0, 0), (616, 0), (616, 616), (0, 616)], dtype=np.float32)
+
+            # Get the perspective transform matrix
+            matrix = cv2.getPerspectiveTransform(np.float32(points), target_points)
+
+            # Apply the perspective transform
+            result = cv2.warpPerspective(frame, matrix, (616, 616))
+
+            # Display the original and transformed frames side by side
+            cv2.imshow("Original Frame", frame)
+            cv2.imshow("Perspective Transform", result)
+
+        else:
+            cv2.imshow("Perspective Transform", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:  # Press 'Esc' to exit
+            break
+
+    # Release the camera and close all windows
+    # cap.release()
+    cv2.destroyAllWindows()
+
+    # For webcam input:
+    # cap = cv2.VideoCapture(2)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1600)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1200)
+
+    with mp_hands.Hands(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
+
+        # Define the target points for the perspective transform
+        target_points = np.array([(0, 0), (616, 0), (616, 616), (0, 616)], dtype=np.float32)
+        # Get the perspective transform matrix
+        matrix = cv2.getPerspectiveTransform(np.float32(points), target_points)
+
+        while cap.isOpened():
+            success, image = cap.read()
+
+            # Apply the perspective transform
+            result = cv2.warpPerspective(image, matrix, (616, 616))
+
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # Flip the image horizontally for a later selfie-view display, and convert
+            # the BGR image to RGB.
+
+            # Mit Spiegelung
+            # image = cv2.cvtColor(cv2.flip(result, 1), cv2.COLOR_BGR2RGB)
+
+            # Ohne Spiegelung
+            image = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            results = hands.process(image)
+            image_height, image_width, _ = image.shape  # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    # Here is How to Get All the Coordinates
+                    for ids, landmrk in enumerate(hand_landmarks.landmark):
+                        # print(ids, landmrk)
+                        # Nr.4 ist Daumen und Nr. 8 ist Zeigefinger
+                        cx, cy = landmrk.x * image_width, landmrk.y * image_height
+                        if ids == 4:
+                            print('Daumen:', cx, cy)
+                            client.send_message("/daumen", [ids, cx, cy])
+                            clientB.send_message("/daumen", [ids, cx, cy])
+
+                        if ids == 8:
+                            print('Zeigefinger:', cx, cy)
+                            client.send_message("/zeigefinger", [ids, cx, cy])
+                            clientB.send_message("/zeigefinger", [ids, cx, cy])
+
+                        mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            cv2.imshow('MediaPipe Hands', image)
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
